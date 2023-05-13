396a556901e55494d7c6174ca318b9d1cdf23732
Iskhak Asanov <asanov.personal@gmail.com>
2023-05-13 16:16:50 +0600

    fixed type checking

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,24 +1,16 @@
 import supabase from '@/utils/supabase'
 import { notFound } from 'next/navigation'
-import type { Metadata } from 'next';
 
 export const revalidate = 60
-
-export async function generateMetadata({ params: { id } }: { params: { id: string } }): Promise<Metadata> {
-    const { data: posts } = await supabase.from('posts').select('id')
-    
-    const product = posts?.find((post) => post.id === id);
-
-    if (product && 'title' in product) {
-        return { title: String(product.title) };
-    }
-    return { title: 'Sometime ago there was a post'}
-  }
 
 export async function generateStaticParams() {
   const { data: posts } = await supabase.from('posts').select('id')
 
-  return posts?.map(({ id }) => ({
+  if (!posts && !Array.isArray(posts)) {
+    return []
+  }
+
+  return posts.map(({ id }) => ({
     id,
   }))
 }
